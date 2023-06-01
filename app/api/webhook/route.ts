1f98693e38a121379849db2500ac584c11dd7f15
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-02 00:06:39 +0800

    fix: build

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
 });
 
-export async function POST(request: NextRequest, response: NextResponse) {
+export async function POST(request: any, response: NextResponse) {
   let event: Stripe.Event;
   const signature = request.headers.get("stripe-signature");
   const body = await buffer(request.body);
