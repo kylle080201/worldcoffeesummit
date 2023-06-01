193e51d85d518940406458f9171eb23fc4c0c68a
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-01 20:49:57 +0800

    fix: fix build errors

--- app/api/checkout-sessions/route.ts
+++ app/api/checkout-sessions/route.ts
@@ -1,12 +1,12 @@
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
 });
 
-export async function POST(request: { json: () => any }, response: Response) {
+export async function POST(request: NextRequest, response: Response) {
   const req = await request.json();
   const line_items = [req.line_items];
   const formData = [req.formData];
