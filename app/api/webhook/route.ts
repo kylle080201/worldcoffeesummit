07bbd92bee01e423781e7bbcabeed834071350ea
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-01 23:04:15 +0800

    fix: fix no signature 7

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -10,7 +10,8 @@ export async function POST(request: NextRequest, response: NextResponse) {
   let event: Stripe.Event;
   const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
   const signature = request.headers.get("stripe-signature");
-  const body = await request.json();
+  const req = await request.json();
+  const body = JSON.stringify(req);
   try {
     event = stripe.webhooks.constructEvent(body, signature!, webhookSecret);
 
