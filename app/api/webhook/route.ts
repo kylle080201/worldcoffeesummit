d17e2a4c964f5259284361c94a9f8fb0d099df79
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-02 10:06:55 +0800

    refactor: 7

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -10,42 +10,45 @@ export async function POST(request: NextRequest, response: NextResponse) {
   let event: Stripe.Event;
   const signature = request.headers.get("stripe-signature");
 
-  const rawBody = await buffer(await request.json(), { encoding: "utf-8" });
-  const body = rawBody.toString();
+  // const rawBody = await buffer(await request.json(), { encoding: "utf-8" });
+  // const body = rawBody.toString();
 
-  const header = stripe.webhooks.generateTestHeaderString({
-    payload: body,
-    secret: process.env.STRIPE_WEBHOOK_SECRET!,
-  });
-
-  try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature!,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    );
-    console.log("✅ Success:", event.id);
-    if (event.type === "checkout.session.completed") {
-      console.log("💰 Payment Received!");
-      return NextResponse.json({
-        body,
-        header,
-        webhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
-      });
-    }
-  } catch (error: any) {
-    console.log(`❌ Error message: ${error.message}`);
-    return NextResponse.json(
-      {
-        message: error.message,
-        body,
-        header,
-        signature,
-        webhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
-      },
-      {
-        status: 400,
+  await fetch("/api/webhook", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then(async (data) => {
+      try {
+        event = stripe.webhooks.constructEvent(
+          data,
+          signature!,
+          process.env.STRIPE_WEBHOOK_SECRET!
+        );
+        console.log("✅ Success:", event.id);
+        if (event.type === "checkout.session.completed") {
+          console.log("💰 Payment Received!");
+          return NextResponse.json({
+            data,
+            signature,
+            webhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
+          });
+        }
+      } catch (error: any) {
+        console.log(`❌ Error message: ${error.message}`);
+        return NextResponse.json(
+          {
+            message: error.message,
+            data,
+            signature,
+            webhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
+          },
+          {
+            status: 400,
+          }
+        );
       }
-    );
-  }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
