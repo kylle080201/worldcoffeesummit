df55af13bb8d2826d2c12b352340accde6de163c
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-04 23:09:34 +0800

    fix: post payment logic

--- app/api/payment_success/route.ts
+++ app/api/payment_success/route.ts
@@ -2,9 +2,5 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest, response: NextResponse) {
   const req1 = await request.json();
-  console.log(req1);
-  const req2 = await request.json();
-  console.log(req2);
-  response = req2 + req1;
-  return response.json();
+  return NextResponse.json(req1);
 }
