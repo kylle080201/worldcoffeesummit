86c27e0cacdeb810a99dfc249084136acbbdefa7
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-04 23:30:00 +0800

    fix: success and cancel url

--- app/api/checkout-sessions/route.ts
+++ app/api/checkout-sessions/route.ts
@@ -15,8 +15,8 @@ export async function POST(request: NextRequest, response: Response) {
       mode: "payment",
       payment_method_types: ["card"],
       line_items,
-      success_url: `http://localhost:3000/register/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: "http://localhost:3000/register",
+      success_url: `/register/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: "/register",
     });
     const retrievedSession = await stripe.checkout.sessions.retrieve(
       session?.id
