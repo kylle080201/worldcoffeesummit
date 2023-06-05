5632c7acf1ffe8a24dd7d9910de3666c1cc3e90b
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-05 09:37:06 +0800

    fix: 5

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -38,7 +38,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
             message: error.message,
           },
           {
-            status: 400,
+            status: 402,
           }
         );
       }
