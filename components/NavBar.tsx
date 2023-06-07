3707e4489db5fccc4a6e4c0e42e0f846f3d850f2
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-08 00:43:54 +0800

    refactor: removed commented lines

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -84,7 +84,6 @@ function Navbar() {
 
                     <Disclosure.Panel className="md:hidden">
                         <div className="pt-2 pb-3 space-y-1">
-                            {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
                             {navigation.map((item) => (
                                 <Disclosure.Button
                                     as='a'
