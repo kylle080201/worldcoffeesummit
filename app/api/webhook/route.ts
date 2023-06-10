0d00c9858ec8cf7dce07614c3873bfe5013864a8
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-10 10:54:51 +0800

    fix: try async

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
 
   try {
     event = stripe.webhooks.constructEvent(body, header, secret);
-    if (event.type === "checkout.session.completed") {
+    if (event.type === "checkout.session.async_payment_succeeded") {
       const paymentIntentId = req.data.object.payment_intent;
       const checkoutSessionId = req.data.object.id;
 
