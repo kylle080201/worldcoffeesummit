9d42e5347f7f14ac74256c260e912b84eaf80ba6
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-11 16:33:09 +0800

    fix: 6

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
 
   try {
     event = stripe.webhooks.constructEvent(body, header, secret);
-    if (req.type === "checkout.session.completed") {
+    if (event.type === "checkout.session.completed") {
       const paymentIntentId = await req.data.object.payment_intent;
       const checkoutSessionId = req.data.object.id;
 
