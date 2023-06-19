b5f8376abce3b27d1b8aa4432300bd3eb69b92a0
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-06-19 08:34:18 +0800

    fix: 3

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,7 +2,6 @@ import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 import connectMongo from "../../../utils/mongodb";
 import Tickets from "../../../models/tickets";
-import { buffer } from "micro";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
@@ -13,13 +12,12 @@ const secret = process.env.STRIPE_WEBHOOK_SECRET!;
 export async function POST(request: NextRequest, response: NextResponse) {
   let event: Stripe.Event;
   const req = await request.json();
-  const body = await buffer(request);
   const signature = request.headers.get("stripe-signature") as string;
   const paymentIntentId = await req.data.object.payment_intent;
   const checkoutSessionId = await req.data.object.id;
 
   try {
-    event = stripe.webhooks.constructEvent(body, signature, secret);
+    event = stripe.webhooks.constructEvent(req, signature, secret);
     if (event.type === "checkout.session.completed") {
       await connectMongo();
 
