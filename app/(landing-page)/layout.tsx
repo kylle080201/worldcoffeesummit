c1d72853c6f2f3c83cee3d16d2f9af85f717676e
Kylle Varon Madrigal <jp.madrigal07@gmail.com>
2023-08-06 23:33:36 +0800

    fix: meta data

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         <link rel="icon" href=
           "https://worldcoffeealliance.com/wp-content/uploads/2023/06/world-coffee-summit-icon.png"
           type="image/x-icon" />
-        <meta property="og:title" content="World Coffee Summit 202" />
+        <meta property="og:title" content="World Coffee Summit 2023" />
         <meta property="og:description" content="A face-to-face Coffee Summit Exhibition on September 13, 2023" />
         <meta property="og:image" content="https://worldcoffeealliance.com/wp-content/uploads/2023/06/world-coffee-summit-icon.png" />
       </head>
