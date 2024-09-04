fae8d1a71c9299f253c5e7f3b37dec3a57179f76
Kylle Madrigal <kv.madrigal08@gmail.com>
2024-09-04 14:22:18 +0800

    fix: revert autoplay

--- components/WhoYoullMeet.tsx
+++ components/WhoYoullMeet.tsx
@@ -70,7 +70,7 @@ const WhoYoullMeet: React.FC = () => {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 1,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000, 
     cssEase: "linear",
     nextArrow: <NextArrow />,
