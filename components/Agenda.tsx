a727dbc668096bf3ed028c1b10e184525fe6b0bc
Kylle Madrigal <kv.madrigal08@gmail.com>
2024-09-06 00:34:31 +0800

    fix: agenda bg color

--- components/Agenda.tsx
+++ components/Agenda.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Agenda() {
     return (
-        <div id="agenda" className='w-full'>
+        <div id="agenda" className='w-full bg-gray-100'>
             <div className='py-20 max-w-5xl mx-auto'>
                 <h2 className="text-4xl font-bold text-center tracking-tight">AGENDA</h2>
                 {/* agenda 1 */}
