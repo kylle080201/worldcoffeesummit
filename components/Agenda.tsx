a82e447a194c07c7c3c8ddc3fc9c0f446e115bf0
Kylle Madrigal <kv.madrigal08@gmail.com>
2024-10-09 22:57:14 +0800

    fix: agenda wrong color

--- components/Agenda.tsx
+++ components/Agenda.tsx
@@ -549,10 +549,10 @@ function Agenda() {
 
                 <div className='mt-8'>
                     <div className='w-full grid grid-cols-10 gap-x-4 h-max'>
-                        <div className='w-full text-white font-medium col-span-2 justify-center bg-gray-600 mx-auto flex items-center px-6'>
+                        <div className='w-full text-white font-medium col-span-2 justify-center bg-lime-700 mx-auto flex items-center px-6'>
                             10:15
                         </div>
-                        <div className='w-full text-white font-medium col-span-8 bg-gray-600 mx-auto flex items-center px-6 py-2'>
+                        <div className='w-full text-white font-medium col-span-8 bg-lime-700 mx-auto flex items-center px-6 py-2'>
                             AGRI-DATA SOLUTION PRESENTATION
                         </div>
                     </div>
