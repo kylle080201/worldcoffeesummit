39e0c6e4274d856d133cc1595426307945084a7d
Kylle Madrigal <kv.madrigal08@gmail.com>
2025-03-25 21:23:21 +0800

    fix: amendments

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -14,12 +14,12 @@ export default function Overview() {
                 {/* Icon in the top right corner */}
                 <div className="absolute top-6 right-6 z-10">
                     <Link href={"https://worldcoffeealliance.com/"} target='_blank'>
-                        <img src="https://worldcoffeealliance.com/wp-content/uploads/2021/08/WCA_new_logo.png" alt="Icon" className="w-24 h-24 md:w-32 md:h-32 xlw-44 xl:h-44" />
+                        <img src="https://worldcoffeealliance.com/wp-content/uploads/2021/08/WCA_new_logo.png" alt="Icon" className="w-24 h-24 md:w-32 md:h-32 xl:w-44 xl:h-44" />
                     </Link>
                 </div>
 
                 {/* Main Container */}
-                <div className="relative flex flex-col items-center text-center text-white mt-32 md:mt-2 xl:mt-0">
+                <div className="relative flex flex-col items-center text-center text-white mt-32 md:mt-2 xl:-mt-8">
                     {/* Event Title */}
                     <h1 className="text-[3rem] md:text-[4rem] xl:text-[6rem] font-bold tracking-tight drop-shadow-xl">
                         THE BIGGER PICTURE
@@ -30,14 +30,14 @@ export default function Overview() {
 
                     <div className="flex flex-col sm:flex-row gap-4 mt-[4rem] md:mt-[5rem] xl:mt-[25rem]">
                         <Link href="/interest">
-                            <button className="bg-orange-400 text-yellow-900 hover:underline hover:drop-shadow-xl font-bold text-lg md:text-xl xl:text-5xl py-1 md:py-2 xl:py-3 px-6 rounded-md">
+                            <button className="bg-[#e1be77ae] text-yellow-900 hover:underline hover:drop-shadow-xl font-bold text-lg md:text-xl xl:text-5xl py-1 md:py-2 xl:py-3 px-6 rounded-md">
                                 SAVE THE DATE & REGISTER YOUR INTEREST
                             </button>
                         </Link>
                     </div>
 
                     {/* Event Logos - Smaller Size */}
-                    <div className="mt-6 flex flex-col sm:flex-row items-center bg-yellow-200/40 p-2 md:p-4w xl:p-6 rounded-md shadow-md gap-4">
+                    <div className="mt-6 flex flex-col sm:flex-row items-center bg-[#e1be77ae] p-2 md:p-4w xl:p-6 rounded-md shadow-md gap-4">
                         <div className="p-2 bg-lime-700 rounded-md w-fit text-md md:text-xl xl:text-3xl font-bold tracking-wide">
                             23-24 October 2025 <span className="block sm:inline">| London</span>
                         </div>
