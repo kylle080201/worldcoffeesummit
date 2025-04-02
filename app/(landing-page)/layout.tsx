7fcac978e616e83cf1891f3bb8ef333d22f9d169
Kylle Madrigal <kv.madrigal08@gmail.com>
2025-04-02 19:18:28 +0800

    change year to 2025

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <link rel="icon" href=
           "https://worldcoffeealliance.com/wp-content/uploads/2023/06/world-coffee-summit-icon.png"
           type="image/x-icon" />
-        <meta property="og:title" content="World Coffee Innovation Summit 2024" />
+        <meta property="og:title" content="World Coffee Innovation Summit 2025" />
         <meta property="og:description" content="As the leading summit that connects COFFEE, CLIMATE CHANGE and FINANCE through INNOVATION, the World Coffee Innovation Summit is a must-attend for those who invested in advancing nature-positive, resilient agriculture and food systems for a net positive future in coffee sector. Join global coffee, climate and finance leaders and agri-food innovators in London for two days of in-depth discussions and cross-sector networking." />
         <meta property="og:image" content="https://worldcoffeealliance.com/wp-content/uploads/2024/04/world-coffee-innovation-summit-high-resolution-logo-transparent-1.png" />
       </head>
