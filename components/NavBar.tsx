be8f577b21918d0ec52347a18311da2d70f94eb9
Kylle Madrigal <kv.madrigal08@gmail.com>
2025-04-29 23:11:22 +0800

    for preview

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -96,10 +96,10 @@ function Navbar() {
                                                 Exhibit/Sponsor
                                             </button> */}
                                             <button
-                                                onClick={() => router.push('/interest')}
+                                                onClick={() => router.push('/register/form')}
                                                 className="relative inline-flex gap-x-1.5 rounded-md bg-lime-900 hover:bg-lime-800 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                                             >
-                                                Register Interest
+                                                Register
                                             </button>
                                         </div>
                                     </div>
