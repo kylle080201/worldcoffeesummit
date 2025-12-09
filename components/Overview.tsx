57d5b9daea1284ef7f65dd2134ff2a121976308f
ali-nywebforum <ali@nywebforum.com>
2025-12-10 00:50:09 +0500

    Update Overview.tsx

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -43,12 +43,15 @@ export default function Overview() {
                     <p className=" mt-2 text-2xl sm:text-3xl md:text-4xl font-bold max-w-5xl drop-shadow-xl">
                       Unlocking Coffee & Cocoa Synergies Through Innovation
 
-                    </p>                        
+                    </p>            
+                   <h1 className=" text-yellow-300 text-[3rem] md:text-[4rem] xl:text-[6rem] font-bold tracking-tight drop-shadow-xl">
+                        SAVE THE DATE
+                    </h1>            
 
                     {/* Event Logos - Smaller Size */}
                     <div className="flex flex-col sm:flex-row items-center p-2 rounded-md  gap-2 mt-[4rem] md:mt-[5rem] xl:mt-[10rem] my-14">
                         <div className="p-2 rounded-md w-fit text-lg md:text-2xl xl:text-4xl font-bold tracking-wide">
-                            23-24 October 2025 <span className="block sm:inline">| QEII CENTRE London | #WCIS25</span>
+                           21-22 OCTOBER 2026 <span className="block sm:inline">| QEII CENTRE & HOUSE OF LORDS, LONDON | #WCIS26</span>
                         </div>
                     </div>
 
