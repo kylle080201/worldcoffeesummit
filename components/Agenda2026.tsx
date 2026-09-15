bf0495e359d97a4e3662b2a5640ba1b033038af9
Kylle Madrigal <kv.madrigal08@gmail.com>
2026-09-15 19:07:58 +0800

    Add Dominic Probyn as a new speaker in Speakers2026 component and update Agenda2026 to include him in day 1 lineup. Include associated images for branding consistency and enhance speaker details with a description.

--- components/Agenda2026.tsx
+++ components/Agenda2026.tsx
@@ -209,7 +209,8 @@ const day1: DayAgenda = {
       speakers: pick(
         "Anup Jagwani",
         "Dr. Sarah Tischer",
-        "Saurabh Sharma"
+        "Saurabh Sharma",
+        "Dominic Probyn"
       ),
     },
     {
