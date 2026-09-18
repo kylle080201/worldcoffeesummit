f64181f0971f8c1b0c8d7bc916fb66717b306bf6
Kylle Madrigal <kv.madrigal08@gmail.com>
2026-09-18 20:24:53 +0800

    Update Bryce Cronkite-Ratcliff's image in Speakers2026 component to a new file for improved quality and consistency.

--- components/Speakers2026.tsx
+++ components/Speakers2026.tsx
@@ -38,7 +38,7 @@ import adamJasonImage from '../images/2026-speakers/GCC-Adam-Jason/GCC_Adam Jaso
 import gccLogo from '../images/2026-speakers/GCC-Adam-Jason/LOGO-original-GCC (1).png'
 import patrickLanzImage from '../images/2026-speakers/Osapiens-Tera-Patrick-Lanz/image1007 copy.jpg'
 import osapiensLogo from '../images/2026-speakers/Osapiens-Tera-Patrick-Lanz/logo_osapiens_horizontal_halfblack.png'
-import bryceCronkiteRatcliffImage from '../images/2026-speakers/Google-Bryce-Cronkite-Ratcliff/Screenshot 2026-09-13 at 22.37.53.png'
+import bryceCronkiteRatcliffImage from '../images/2026-speakers/Google-Bryce-Cronkite-Ratcliff/IMG_6591 copy.jpeg'
 import googleLogo from '../images/2026-speakers/Google-Bryce-Cronkite-Ratcliff/Google Logo.png'
 import Link from 'next/link'
 const anupImage = '/images/2026-speakers/anup-jagwani.jpg'
